34faf5fef6dfda619599454e03424dfa3f20a907
Ruan vitor <72428158+Ruan-codeVi@users.noreply.github.com>
2021-05-18 22:14:40 -0300

    listas
    trabalhando com listas

--- meu-site/src/App.js
+++ meu-site/src/App.js
@@ -1,14 +1,43 @@
 import React, {Component} from 'react';
-import Membro from './components/Membro'
+// import Membro from './components/Membro'
+import Feed from './components/Feed';
+
+// LISTAS
 
 class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            feed: [
+                {id:1, username:"Matheus", curtidas:10, comentarios: 14},
+                {id:2, username:"Lucas", curtidas:120, comentarios: 44},
+                {id:3, username:"Joao", curtidas:6, comentarios: 3},
+                {id:4, username:"Kleber", curtidas:1, comentarios: 0}
+            ]
+        }
+      
+    }
+
+   
+
+
     render(){
         return(
-            <div>
-                <Membro nome="visitantte"/>
+            <div >
+                {this.state.feed.map((item)=>{
+                    return(
+                        <Feed key={item.id} username={item.username} //Passando as props para Feed
+                        curtidas={item.curtidas} comentarios={item.comentarios}/>
+                    );
+                })}
+               
             </div>
         );
     }
 }
+
+    
+
 
 export default App;
